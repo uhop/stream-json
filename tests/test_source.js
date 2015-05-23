3665560a096aed951530b8686223c7e0c5f3aa37
Eugene Lazutkin <eugene@lazutkin.com>
2015-05-23 10:19:17 -0500

    Separated Counter from a test.

--- tests/test_source.js
+++ tests/test_source.js
@@ -6,6 +6,7 @@ var unit = require("heya-unit");
 var fs = require("fs"), path = require("path"), zlib = require("zlib");
 
 var createSource = require("../main");
+var Counter = require("./Counter");
 
 
 unit.add(module, [
@@ -36,7 +37,7 @@ unit.add(module, [
 				if(err){ throw err; }
 
 				var o = JSON.parse(data);
-				walk(o, plainCounter);
+				Counter.walk(o, plainCounter);
 
 				fs.createReadStream(path.resolve(__dirname, "./sample.json.gz")).
 					pipe(zlib.createGunzip()).pipe(source.input);
@@ -44,49 +45,3 @@ unit.add(module, [
 		});
 	}
 ]);
-
-
-function Counter(){
-	this.objects = 0;
-	this.keys    = 0;
-	this.arrays  = 0;
-	this.nulls   = 0;
-	this.trues   = 0;
-	this.falses  = 0;
-	this.numbers = 0;
-	this.strings = 0;
-}
-
-function walk(o, counter){
-	switch(typeof o){
-		case "string":
-			++counter.strings;
-			return;
-		case "number":
-			++counter.numbers;
-			return;
-		case "boolean":
-			if(o){
-				++counter.trues;
-			}else{
-				++counter.falses;
-			}
-			return;
-	}
-	if(o === null){
-		++counter.nulls;
-		return;
-	}
-	if(o instanceof Array){
-		++counter.arrays;
-		o.forEach(function(o){ walk(o, counter); });
-		return;
-	}
-	++counter.objects;
-	for(var key in o){
-		if(o.hasOwnProperty(key)){
-			++counter.keys;
-			walk(o[key], counter);
-		}
-	}
-}
