181ca2198380828d3cb0c7601ace3f44f6b2e95a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-06-10 20:33:47 -0500

    Returned Emitter.

---
+++ utils/Emitter.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const {Writable} = require('stream');
+
+class Emitter extends Writable {
+  static emitter(options) {
+    return new Emitter(options);
+  }
+
+  constructor(options) {
+    super(Object.assign({}, options, {objectMode: true}));
+  }
+
+  _write(chunk, encoding, callback) {
+    this.emit(chunk.name, chunk.value);
+    callback(null);
+  }
+}
+Emitter.make = Emitter.emitter;
+
+module.exports = Emitter;
