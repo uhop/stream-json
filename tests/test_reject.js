63461012e259be45e95cc9adfb762ce3b3830d98
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-06-12 17:38:45 -0500

    Added Reject + tests.

---
+++ tests/test_reject.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const unit = require('heya-unit');
+const {chain} = require('stream-chain');
+
+const {parser} = require('../Parser');
+const {streamArray} = require('../utils/StreamArray');
+const {reject} = require('../utils/Reject');
+
+const {readString} = require('./ReadString');
+
+unit.add(module, [
+  function test_reject_events(t) {
+    const async = t.startAsync('test_reject_events');
+
+    const input = [{a: {}}, {b: []}, {c: null}, {d: 1}, {e: 'e'}],
+      pipeline = chain([readString(JSON.stringify(input)), parser(), reject({filter: stack => stack[0] % 2})]),
+      expected = [
+        'startArray',
+        'startObject',
+        'startKey',
+        'stringChunk',
+        'endKey',
+        'startObject',
+        'endObject',
+        'endObject',
+        'nullValue',
+        'startObject',
+        'startKey',
+        'stringChunk',
+        'endKey',
+        'nullValue',
+        'endObject',
+        'nullValue',
+        'startObject',
+        'startKey',
+        'stringChunk',
+        'endKey',
+        'startString',
+        'stringChunk',
+        'endString',
+        'endObject',
+        'endArray'
+      ],
+      result = [];
+
+    pipeline.on('data', chunk => result.push(chunk.name));
+    pipeline.on('end', () => {
+      eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  },
+  function test_pick_objects(t) {
+    const async = t.startAsync('test_pick_objects');
+
+    const input = [{a: {}}, {b: []}, {c: null}, {d: 1}, {e: 'e'}],
+      pipeline = chain([readString(JSON.stringify(input)), parser({packValues: true}), reject({filter: stack => stack[0] % 2}), streamArray()]),
+      expected = [{a: {}}, null, {c: null}, null, {e: 'e'}],
+      result = [];
+
+    pipeline.on('data', chunk => result.push(chunk.value));
+    pipeline.on('end', () => {
+      eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  },
+  function test_pick_objects_string_filter(t) {
+    const async = t.startAsync('test_pick_objects_string_filter');
+
+    const input = [{a: {}}, {b: []}, {c: null}, {d: 1}, {e: 'e'}],
+      pipeline = chain([readString(JSON.stringify(input)), parser({packValues: true}), reject({filter: '1'}), streamArray()]),
+      expected = [{a: {}}, null, {c: null}, {d: 1}, {e: 'e'}],
+      result = [];
+
+    pipeline.on('data', chunk => result.push(chunk.value));
+    pipeline.on('end', () => {
+      eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  },
+  function test_pick_objects_regexp_filter(t) {
+    const async = t.startAsync('test_pick_objects_regexp_filter');
+
+    const input = [{a: {}}, {b: []}, {c: null}, {d: 1}, {e: 'e'}],
+      pipeline = chain([readString(JSON.stringify(input)), parser({packValues: true}), reject({filter: /\b[1-5]\.[a-d]\b/}), streamArray()]),
+      expected = [{a: {}}, {b: null}, {c: null}, {d: null}, {e: 'e'}],
+      result = [];
+
+    pipeline.on('data', chunk => result.push(chunk.value));
+    pipeline.on('end', () => {
+      eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  },
+  function test_pick_empty(t) {
+    const async = t.startAsync('test_pick_empty');
+
+    const input = [{a: {}}, {b: []}, {c: null}, {d: 1}, {e: 'e'}],
+      pipeline = chain([readString(JSON.stringify(input)), parser({packValues: true}), reject({filter: stack => stack.length}), streamArray()]),
+      expected = [null, null, null, null, null],
+      result = [];
+
+    pipeline.on('data', chunk => result.push(chunk.value));
+    pipeline.on('end', () => {
+      eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  },
+  function test_pick_objects_once(t) {
+    const async = t.startAsync('test_pick_objects_regexp_filter');
+
+    const input = [{a: {}}, {b: []}, {c: null}, {d: 1}, {e: 'e'}],
+      pipeline = chain([readString(JSON.stringify(input)), parser({packValues: true}), reject({filter: /\b[1-5]\.[a-d]\b/, rejectOnce: true}), streamArray()]),
+      expected = [{a: {}}, {b: null}, {c: null}, {d: 1}, {e: 'e'}],
+      result = [];
+
+    pipeline.on('data', chunk => result.push(chunk.value));
+    pipeline.on('end', () => {
+      eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  }
+]);
---
+++ utils/Reject.js
@@ -0,0 +1,172 @@
+'use strict';
+
+const {Transform} = require('stream');
+const withParser = require('./withParser');
+
+const stringFilter = (string, separator) => stack => {
+  const path = stack.join(separator);
+  return (
+    (path.length === string.length && path === string) ||
+    (path.length > string.length && path.substr(0, string.length) === string && path.substr(string.length, separator.length) === separator)
+  );
+};
+
+const regExpFilter = (regExp, separator) => stack => regExp.test(stack.join(separator));
+
+class Reject extends Transform {
+  static make(options) {
+    return new Reject(options);
+  }
+
+  static withParser(options) {
+    return withParser(Reject.make, options);
+  }
+
+  constructor(options) {
+    super(Object.assign({}, options, {writableObjectMode: true, readableObjectMode: true}));
+    this._transform = this._check;
+    this._once = options && options.rejectOnce;
+    this._stack = [];
+
+    const filter = options && options.filter,
+      separator = (options && options.pathSeparator) || '.';
+    if (typeof filter == 'string') {
+      this.filter = stringFilter(filter, separator);
+      this._once = true;
+    } else if (typeof filter == 'function') {
+      this.filter = filter;
+    } else if (filter instanceof RegExp) {
+      this.filter = regExpFilter(filter, separator);
+    } else {
+      this.filter = () => true;
+    }
+  }
+
+  _check(chunk, encoding, callback) {
+    // update the last stack key
+    switch (chunk.name) {
+      case 'startObject':
+      case 'startArray':
+      case 'startString':
+      case 'startNumber':
+      case 'nullValue':
+      case 'trueValue':
+      case 'falseValue':
+        if (typeof this._stack[this._stack.length - 1] == 'number') {
+          // array
+          ++this._stack[this._stack.length - 1];
+        }
+        break;
+      case 'keyValue':
+        this._stack[this._stack.length - 1] = chunk.value;
+        break;
+    }
+    // check, if we remove a value
+    switch (chunk.name) {
+      case 'startObject':
+      case 'startArray':
+        if (this.filter(this._stack, chunk)) {
+          this._transform = this._skipObject;
+          this._depth = 1;
+          return callback(null);
+        }
+        break;
+      case 'startString':
+        if (this.filter(this._stack, chunk)) {
+          this._transform = this._skipString;
+          return callback(null);
+        }
+        break;
+      case 'startNumber':
+        if (this.filter(this._stack, chunk)) {
+          this._transform = this._skipNumber;
+          return callback(null);
+        }
+        break;
+      case 'nullValue':
+      case 'trueValue':
+      case 'falseValue':
+        if (this.filter(this._stack, chunk)) {
+          this.push({name: 'nullValue', value: null});
+          this._transform = this._once ? this._pass : this._check;
+          return callback(null);
+        }
+        break;
+    }
+    // update the stack
+    switch (chunk.name) {
+      case 'startObject':
+        this._stack.push(null);
+        break;
+      case 'startArray':
+        this._stack.push(-1);
+        break;
+      case 'endObject':
+      case 'endArray':
+        this._stack.pop();
+        break;
+    }
+    this.push(chunk);
+    callback(null);
+  }
+
+  _skipObject(chunk, encoding, callback) {
+    switch (chunk.name) {
+      case 'startObject':
+      case 'startArray':
+        ++this._depth;
+        break;
+      case 'endObject':
+      case 'endArray':
+        --this._depth;
+        break;
+    }
+    if (!this._depth) {
+      this.push({name: 'nullValue', value: null});
+      this._transform = this._once ? this._pass : this._check;
+    }
+    callback(null);
+  }
+
+  _skipString(chunk, encoding, callback) {
+    if (this._expected) {
+      const expected = this._expected;
+      this._expected = '';
+      this._transform = this._once ? this._pass : this._check;
+      this.push({name: 'nullValue', value: null});
+      if (expected !== chunk.name) {
+        return this._transform(chunk, encoding, callback);
+      }
+    } else {
+      if (chunk.name === 'endString') {
+        this._expected = 'stringValue';
+      }
+    }
+    callback(null);
+  }
+
+  _skipNumber(chunk, encoding, callback) {
+    if (this._expected) {
+      const expected = this._expected;
+      this._expected = '';
+      this._transform = this._once ? this._pass : this._check;
+      this.push({name: 'nullValue', value: null});
+      if (expected !== chunk.name) {
+        return this._transform(chunk, encoding, callback);
+      }
+    } else {
+      if (chunk.name === 'endNumber') {
+        this._expected = 'numberValue';
+      }
+    }
+    callback(null);
+  }
+
+  _pass(chunk, encoding, callback) {
+    this.push(chunk);
+    callback(null);
+  }
+}
+Reject.reject = Reject.make;
+
+module.exports = Reject;
