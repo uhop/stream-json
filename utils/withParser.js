0d25e426184b98c7d02fd465380a5cd4fa84d348
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-06-14 11:24:12 -0500

    More simplification.

--- utils/withParser.js
+++ utils/withParser.js
@@ -4,6 +4,7 @@ const Chain = require('stream-chain');
 
 const Parser = require('../Parser');
 
-const withParser = (fn, options) => new Chain([new Parser(options), fn(options)], Object.assign({}, options, {writableObjectMode: false, readableObjectMode: true}));
+const withParser = (fn, options) =>
+  new Chain([new Parser(options), fn(options)], Object.assign({}, options, {writableObjectMode: false, readableObjectMode: true}));
 
 module.exports = withParser;
