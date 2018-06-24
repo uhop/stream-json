10ef26d2c2d71392f56ddf62ccb47231bb8ed7ee
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-06-24 01:54:32 -0500

    Simplified code.

--- utils/emit.js
+++ utils/emit.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const emit = stream => {
-  stream.on('data', item => stream.emit(item.name, item.value));
-  return stream;
-};
+const emit = stream => stream.on('data', item => stream.emit(item.name, item.value));
 
 module.exports = emit;
