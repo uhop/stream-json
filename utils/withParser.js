1ce44cabb94e2d7f6913e9bab27dae2da57f96ba
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-26 12:13:31 -0500

    Minor modernization.

--- utils/withParser.js
+++ utils/withParser.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const Chain = require('stream-chain');
+const {chain} = require('stream-chain');
 
 const Parser = require('../Parser');
 
 const withParser = (fn, options) =>
-  new Chain([new Parser(options), fn(options)], Object.assign({}, options, {writableObjectMode: false, readableObjectMode: true}));
+  chain([new Parser(options), fn(options)], Object.assign({}, options, {writableObjectMode: false, readableObjectMode: true}));
 
 module.exports = withParser;
