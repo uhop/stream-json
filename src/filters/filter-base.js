edfc15005c67c58b03561c261be3a3bd4c2ad72d
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-01-21 22:16:00 -0600

    Refactored by creating `makeStackDiffer()`.

--- src/filters/filter-base.js
+++ src/filters/filter-base.js
@@ -259,5 +259,83 @@ const filterBase =
     });
   };
 
+const makeStackDiffer =
+  (previousStack = []) =>
+  (stack, chunk, options) => {
+    const returnTokens = [];
+
+    // find the common part
+    let commonLength = 0;
+    for (const n = Math.min(stack.length, previousStack.length); commonLength < n && stack[commonLength] === previousStack[commonLength]; ++commonLength);
+
+    // close old objects
+    for (let i = previousStack.length - 1; i > commonLength; --i) {
+      returnTokens.push({name: typeof previousStack[i] == 'number' ? 'endArray' : 'endObject'});
+    }
+
+    // update the index
+    if (commonLength < previousStack.length) {
+      if (commonLength < stack.length) {
+        const key = stack[commonLength];
+        if (typeof key == 'string') {
+          if (options?.streamKeys) {
+            returnTokens.push({name: 'startKey'}, {name: 'stringChunk', value: key}, {name: 'endKey'});
+          }
+          if (options?.packKeys || !options?.streamKeys) {
+            returnTokens.push({name: 'keyValue', value: key});
+          }
+        } else if (typeof key == 'number' && options?.skippedArrayValue) {
+          for (let i = Math.max(0, previousStack[commonLength] + 1); i < key; ++i) {
+            returnTokens.push(...options.skippedArrayValue);
+          }
+        }
+        previousStack[commonLength] = key;
+        ++commonLength;
+      } else {
+        returnTokens.push({name: typeof previousStack[commonLength] == 'number' ? 'endArray' : 'endObject'});
+      }
+    }
+
+    // remove old elements
+    previousStack.splice(commonLength);
+
+    // open new objects
+    for (let i = commonLength; i < stack.length; ++i) {
+      const key = stack[i];
+      previousStack.push(key);
+      if (typeof key == 'number') {
+        if (key >= 0) {
+          returnTokens.push({name: 'startArray'});
+          if (options?.skippedArrayValue) {
+            for (let j = 0; j < key; ++j) {
+              returnTokens.push(...options.skippedArrayValue);
+            }
+          }
+        }
+      } else if (typeof key == 'string') {
+        returnTokens.push({name: 'startObject'});
+        if (options?.streamKeys) {
+          returnTokens.push({name: 'startKey'}, {name: 'stringChunk', value: key}, {name: 'endKey'});
+        }
+        if (options?.packKeys || !options?.streamKeys) {
+          returnTokens.push({name: 'keyValue', value: key});
+        }
+      }
+    }
+
+    // save the stack
+    switch (chunk?.name) {
+      case 'startObject':
+        previousStack.push(null);
+        break;
+      case 'startArray':
+        previousStack.push(-1);
+        break;
+    }
+
+    return many(returnTokens);
+  };
+
 module.exports = filterBase;
 module.exports.filterBase = filterBase;
+module.exports.makeStackDiffer = makeStackDiffer;
--- src/filters/filter.js
+++ src/filters/filter.js
@@ -4,87 +4,16 @@
 
 const {many, none} = require('stream-chain');
 
-const filterBase = require('./filter-base.js');
+const {filterBase, makeStackDiffer} = require('./filter-base.js');
 const withParser = require('../utils/with-parser.js');
 
 const filter = options => {
-  const specialAction = options?.acceptObjects ? 'accept' : 'accept-token';
-  let previousStack = [];
+  const specialAction = options?.acceptObjects ? 'accept' : 'accept-token',
+    stackDiffer = makeStackDiffer();
   return filterBase({
     specialAction,
     transition(stack, chunk, _action, options) {
-      const returnTokens = [];
-
-      // find the common part
-      let commonLength = 0;
-      for (const n = Math.min(stack.length, previousStack.length); commonLength < n && stack[commonLength] === previousStack[commonLength]; ++commonLength);
-
-      // close old objects
-      for (let i = previousStack.length - 1; i > commonLength; --i) {
-        returnTokens.push({name: typeof previousStack[i] == 'number' ? 'endArray' : 'endObject'});
-      }
-
-      // update the index
-      if (commonLength < previousStack.length) {
-        if (commonLength < stack.length) {
-          const key = stack[commonLength];
-          if (typeof key == 'string') {
-            if (options?.streamKeys) {
-              returnTokens.push({name: 'startKey'}, {name: 'stringChunk', value: key}, {name: 'endKey'});
-            }
-            if (options?.packKeys || !options?.streamKeys) {
-              returnTokens.push({name: 'keyValue', value: key});
-            }
-          } else if (typeof key == 'number' && options?.skippedArrayValue) {
-            for (let i = Math.max(0, previousStack[commonLength] + 1); i < key; ++i) {
-              returnTokens.push(...options.skippedArrayValue);
-            }
-          }
-          previousStack[commonLength] = key;
-          ++commonLength;
-        } else {
-          returnTokens.push({name: typeof previousStack[commonLength] == 'number' ? 'endArray' : 'endObject'});
-        }
-      }
-
-      // remove old elements
-      previousStack.splice(commonLength);
-
-      // open new objects
-      for (let i = commonLength; i < stack.length; ++i) {
-        const key = stack[i];
-        previousStack.push(key);
-        if (typeof key == 'number') {
-          if (key >= 0) {
-            returnTokens.push({name: 'startArray'});
-            if (options?.skippedArrayValue) {
-              for (let j = 0; j < key; ++j) {
-                returnTokens.push(...options.skippedArrayValue);
-              }
-            }
-          }
-        } else if (typeof key == 'string') {
-          returnTokens.push({name: 'startObject'});
-          if (options?.streamKeys) {
-            returnTokens.push({name: 'startKey'}, {name: 'stringChunk', value: key}, {name: 'endKey'});
-          }
-          if (options?.packKeys || !options?.streamKeys) {
-            returnTokens.push({name: 'keyValue', value: key});
-          }
-        }
-      }
-
-      // save the stack
-      switch (chunk?.name) {
-        case 'startObject':
-          previousStack.push(null);
-          break;
-        case 'startArray':
-          previousStack.push(-1);
-          break;
-      }
-
-      return many(returnTokens);
+      return stackDiffer(stack, chunk, options);
     }
   })(options);
 };
